81ee04c1624fa2444ea1691252004fa49753c168
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2025-06-12 13:27:40 +0100

    add sortDatesAscending utility to order revision dates

--- common.mjs
+++ common.mjs
@@ -1,3 +1,8 @@
 export function getUserIDs() {
   return ["1", "2", "3", "4", "5"];
 }
+
+
+export function sortDatesAscending(dates) {
+  return [...dates].sort((a, b) => new Date(a) - new Date(b));
+}
