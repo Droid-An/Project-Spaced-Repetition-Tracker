fa2dbc207409ff25c6bfc44e624886fa4076213f
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2025-06-12 13:25:44 +0100

    add isFutureDate utility to filter past agenda items

--- common.mjs
+++ common.mjs
@@ -1,3 +1,14 @@
 export function getUserIDs() {
   return ["1", "2", "3", "4", "5"];
 }
+
+
+export function isFutureDate(date) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); 
+
+  const inputDate = new Date(date);
+  inputDate.setHours(0, 0, 0, 0); 
+
+  return inputDate >= today;
+}
