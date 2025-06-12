d0fdaa6081357039dccefcc95562939dcfae2277
Droid-An <andrei.filippov.aaf@gmail.com>
2025-06-12 23:13:08 +0100

    added test example

--- common.test.mjs
+++ common.test.mjs
@@ -1,15 +1,10 @@
-import { getDateInAString, getUserIDs } from "./common.mjs";
+import { getDateInAString } from "./common.mjs";
 
+//test example
 describe("getDateInAString", () => {
   test("should calculate correct revision dates for a given topic and start date", () => {
     const r = getDateInAString(1);
     console.log(r);
     expect(r).toBe('1970-01-01')
-  });
-});
-
-describe("default", ()=> {
-  test("User count is correct", () => {
-    expect(getUserIDs()).toHaveLength(5);
   });
 });
